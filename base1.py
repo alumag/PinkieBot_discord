3427d2ba143e35ea5e46d2e0b04cce06a83ab3c1
Shahar Rosenberg <rose.shahar@gmail.com>
2017-06-12 18:36:26 +0300

    tryouts

--- base1.py
+++ base1.py
@@ -14,13 +14,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith('!test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
-
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+        await client.send_message(message.channel, 'Here is you test message, @' + message.author.name)
     elif message.content.startswith('!sleep'):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
