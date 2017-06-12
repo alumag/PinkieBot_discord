b557e358a1bd040f2146537eb2511116d20a8ef1
Noam <noam.pnueli@gmail.com>
2017-06-12 21:05:14 +0300

    eh

--- ddg_cmd.py
+++ ddg_cmd.py
@@ -7,7 +7,6 @@ import json
 async def query_ddg(client, message, args):
 
 	try:
-		print(args)
 		query_data = urllib.request.urlopen(f'http://api.duckduckgo.com/?q={args}&format=json&pretty=1&skip_disambig=1').read()
 		decoded_data = json.loads(query_data)
 
