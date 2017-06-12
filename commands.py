0e5193571196fe6a80e4490cae83d083179bda1d
Noam <noam.pnueli@gmail.com>
2017-06-12 21:55:13 +0300

    added urban fun

--- commands.py
+++ commands.py
@@ -1,6 +1,7 @@
 from calc_cmd import calc_cmd
 from ddg_cmd import query_ddg
 from rand_cmd import rand_cmd
+from urban_cmd import query_urban_dictionary
 
 
 def help_cmd(client, message, args):
@@ -17,4 +18,5 @@ commands = {
     'calc': calc_cmd,
     'rand': rand_cmd,
     'ddg': query_ddg,
+    'urban': query_urban_dictionary,
 }
---
+++ urban_cmd.py
@@ -0,0 +1,11 @@
+import urllib.request
+import json
+
+def query_urban_dictionary(client, message, args):
+	try:
+		query_data = urllib.request.urlopen(f'http://api.urbandictionary.com/v0/define?term={args}').read()
+		decoded_data = json.loads(query_data)
+
+		return f'**Word:** {decoded_data["list"][0]["word"]}\n {decoded_data["list"][0]["definition"]}'
+	except:
+		return "Sorry, I couldn't find anything on that"
