1b9e57c53fbb84b2457fe49aae4f69d703377814
Noam <noam.pnueli@gmail.com>
2017-06-15 21:57:38 +0300

    supports unicode

--- ddg_cmd.py
+++ ddg_cmd.py
@@ -9,30 +9,30 @@ def is_ascii(s):
 
 def query_ddg(client, message, args):
 
-	# try:
-	if not is_ascii(args):
-		args = urllib.request.quote(args)
-	query_data = urllib.request.urlopen(f'http://api.duckduckgo.com/?q={args}&format=json&pretty=1&skip_disambig=1').read()
-	decoded_data = json.loads(query_data)
-
-	results = decoded_data['Results']
-	abstract = decoded_data['Abstract']
-
-	if not abstract and results:
-		embed = discord.Embed(title=args, description=f'{results[0]["FirstURL"]}', color=3447003)
-	elif abstract and not results:
-		embed = discord.Embed(title=args, description=f'{abstract}', color=3447003)
-	elif abstract and results:
-		embed = discord.Embed(title=f"{results[0]['FirstURL']}", description=f'{abstract}', color=3447003)
-	else:
-		query_data = urllib.request.urlopen(f'http://api.duckduckgo.com/?q=\{args}&format=json&pretty=1&skip_disambig=1&no_redirect=1').read()
+	try:
+		if not is_ascii(args):
+			args = urllib.request.quote(args)
+		query_data = urllib.request.urlopen(f'http://api.duckduckgo.com/?q={args}&format=json&pretty=1&skip_disambig=1').read()
 		decoded_data = json.loads(query_data)
 
-		if decoded_data['Redirect'].startswith('https://api.duckduckgo.com'):
-			return discord.Embed(title='Sorry', description='Could not find matching query', color=0xff5b4c)
+		results = decoded_data['Results']
+		abstract = decoded_data['Abstract']
 
-		embed = discord.Embed(title=f'{decoded_data["Redirect"]}', color=3447003)
+		if not abstract and results:
+			embed = discord.Embed(title=urllib.request.unquote(args), description=f'{results[0]["FirstURL"]}', color=3447003)
+		elif abstract and not results:
+			embed = discord.Embed(title=urllib.request.unquote(args), description=f'{abstract}', color=3447003)
+		elif abstract and results:
+			embed = discord.Embed(title=f"{urllib.request.unquote(results[0]['FirstURL'])}", description=f'{abstract}', color=3447003)
+		else:
+			query_data = urllib.request.urlopen(f'http://api.duckduckgo.com/?q=\{args}&format=json&pretty=1&skip_disambig=1&no_redirect=1').read()
+			decoded_data = json.loads(query_data)
 
-	return embed
-	# except:
-	return discord.Embed(title='Sorry', description='Could not find matching query', color=0xff5b4c)
+			if decoded_data['Redirect'].startswith('https://api.duckduckgo.com'):
+				return discord.Embed(title='Sorry', description='Could not find matching query', color=0xff5b4c)
+
+			embed = discord.Embed(title=f'{urllib.request.unquote(decoded_data["Redirect"])}', color=3447003)
+
+		return embed
+	except:
+		return discord.Embed(title='Sorry', description='Could not find matching query', color=0xff5b4c)
