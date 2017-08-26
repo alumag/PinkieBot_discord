1eb8ae3de3fa02b0324219dfeca0b58e52a44567
Noam <noam.pnueli@gmail.com>
2017-08-26 12:04:13 +0300

    added priv decorator

--- clear_messages.py
+++ clear_messages.py
@@ -4,8 +4,8 @@ import utils
 
 @utils.admin
 async def clear_messages(client, message, args):
-		try:
-			await client.purge_from(message.channel, limit=min(100, int(args)))
-		except Exception as e:
-			print(e)
-			await client.send_message(message.channel, message.author.mention + " can't do that, sorry.")
+	try:
+		await client.purge_from(message.channel, limit=min(100, int(args)))
+	except Exception as e:
+		print(e)
+		await client.send_message(message.channel, message.author.mention + " can't do that, sorry.")
