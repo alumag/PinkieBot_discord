64be537a728ae452e0a997a88d473675f98dab9c
Noam <noam.pnueli@gmail.com>
2017-08-26 12:03:38 +0300

    added priv decorator

--- clear_messages.py
+++ clear_messages.py
@@ -1,7 +1,9 @@
 import discord
+import utils
 
+
+@utils.admin
 async def clear_messages(client, message, args):
-	if message.author.server.owner.top_role in message.author.roles:
 		try:
 			await client.purge_from(message.channel, limit=min(100, int(args)))
 		except Exception as e:
