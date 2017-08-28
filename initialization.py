921dd8d0218e122758ce84dfbd7f60b0a7a03bba
Meitar Reihan <meitarr013@gmail.com>
2017-08-29 00:30:31 +0300

    created initialization.py added support for roles

---
+++ initialization.py
@@ -0,0 +1,88 @@
+import json
+import discord
+
+FILE_NAME = "config.js"
+
+try:
+    open(FILE_NAME).close()
+except IOError:
+    open(FILE_NAME, 'w').close()
+
+
+def init_file(file_name):
+    """
+    write to the file blank config
+
+    :param file_name: the name of the file
+    :type file_name: str
+    :return: None
+    """
+
+    role = discord.Role(server=discord.Server(id=0))
+
+    the_config = {'roles': [role]}
+    with open(file_name, 'w') as file:
+        json.dump(the_config, file, default=to_json, indent=4)
+
+    js = json.dumps(the_config, default=to_json, indent=2)
+    js_role = json.loads(js, object_hook=from_json)
+
+    new_js = json.dumps(js_role, default=to_json, indent=2)
+    print(js == new_js)
+
+
+def to_json(obj):
+    """
+    Convert objects to json format
+
+    :param obj: the object
+    :return: json object as string
+    """
+    if isinstance(obj, discord.Role):
+        [obj.__slots__.remove(to_remove)
+         for to_remove in ['id', 'colour', 'managed', 'server', 'position']
+         if to_remove in obj.__slots__]
+        return \
+            {
+                '__class__': 'discord.Role',
+                '__value__':
+                    {
+                        name: obj.__getattribute__(name)
+                        for name in obj.__slots__
+                    }
+            }
+    elif isinstance(obj, discord.Permissions):
+        return \
+            {
+                '__class__': 'discord.Permissions',
+                '__value__':
+                    {
+                        pram[0]: pram[1]
+                        for pram in obj
+                    }
+            }
+    elif isinstance(obj, discord.colour.Colour):
+        return obj.value
+    raise TypeError(repr(obj) + ' is not JSON serializable')
+
+
+def from_json(obj):
+    """
+    Convert json format to objects
+
+    :param obj: json object as string
+    :return: the object
+    """
+
+    if '__class__' in obj:
+        if obj['__class__'] == 'discord.Role':
+            obj['__value__']['server'] = discord.Server(id=0)
+            return discord.Role(**obj['__value__'])
+        elif obj['__class__'] == 'discord.Permissions':
+            return discord.Permissions(**obj['__value__']).value
+    return obj
+
+
+if __name__ == "__main__":
+    init_file(FILE_NAME)
+
