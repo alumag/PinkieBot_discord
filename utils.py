25f46604f59da2102ba5204409041aeb9dfe5c00
Meitar <meitarr013@gmail.com>
2017-12-10 23:58:32 +0200

    added utils

--- utils.py
+++ utils.py
@@ -1,4 +1,38 @@
+from collections import namedtuple
 from functools import wraps
+
+Command = namedtuple('Command', 'name function channels')
+ChannelName = namedtuple('ChannelName', 'string is_part')
+
+
+def function_registerer():
+    """
+    create a list of functions from all the functions that @ that function
+    name: str, the command that the function will assign to
+    available_at=None: [str], list of channels that the command allowed in,
+                  None = all, #text = exactly that name, text = text as part of the name
+    """
+    functions_list = []
+
+    def registrar_warp(name, available_at=None):
+        if available_at is None:
+            channels = None
+        else:
+            channels = []
+            for ch_name in available_at:
+                if ch_name.startswith('#'):
+                    channels.append(ChannelName(string=ch_name[1:], is_part=False))
+                else:
+                    channels.append(ChannelName(string=ch_name, is_part=True))
+
+        def registrar(func):
+            functions_list.append(Command(name=name, function=func, channels=channels))
+            return func
+
+        return registrar
+
+    registrar_warp.functions_list = functions_list
+    return registrar_warp
 
 
 def admin(func):
@@ -8,3 +42,13 @@ def admin(func):
             return
         return func(client, message, args)
     return wrapped
+
+
+def is_right_channel(name_str, channel_name):
+    if channel_name.is_part:
+        return name_str in channel_name
+    else:
+        return name_str == channel_name.string
+
+
+register_command = function_registerer()
