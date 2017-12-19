2fd5067a3c9e5b2baeb45e20e3fd883f0d2e1459
Aluma Gelbard <10659903+MissLummie@users.noreply.github.com>
2017-12-19 22:07:18 +0200

    move everything into a plugins package

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,19 +1,17 @@
+from urllib.parse import quote_plus
 from uuid import uuid4
 
-from calc_cmd import calc_cmd
-from char_cmds import ord_cmd, chr_cmd
-from ddg_cmd import query_ddg
-from define import define_cmd
-from karma import add_karma_cmd, get_karma_cmd, set_karma_cmd, take_karma_cmd
-from karma_store import buy_item
-from number_bases import hex_to_dec_cmd, dec_to_hex_cmd, num_converter_cmd
-from rand_cmd import rand_cmd
-from urban_cmd import query_urban_dictionary
-from eight_ball_cmd import eight_ball_cmd
-from clear_messages import clear_messages
-from emojiwrite import emojiwrite
-
-from urllib.parse import quote_plus
+from plugins.char_cmds import ord_cmd, chr_cmd
+from plugins.clear_messages import clear_messages
+from plugins.ddg_cmd import query_ddg
+from plugins.define import define_cmd
+from plugins.eight_ball_cmd import eight_ball_cmd
+from plugins.emojiwrite import emojiwrite
+from plugins.karma import add_karma_cmd, get_karma_cmd, set_karma_cmd, take_karma_cmd
+from plugins.karma_store import buy_item
+from plugins.number_bases import hex_to_dec_cmd, dec_to_hex_cmd, num_converter_cmd
+from plugins.rand_cmd import rand_cmd
+from plugins.urban_cmd import query_urban_dictionary
 
 doc_file = open('help_file.txt')
 doc_str = doc_file.read()
@@ -21,7 +19,7 @@ doc_file.close()
 
 
 def help_cmd(client, message, args):
-    msg = "Available commands:\n[" + ", ".join(commands.keys()) + "]"
+    msg = "Available plugins:\n[" + ", ".join(commands.keys()) + "]"
     return msg
 
 
@@ -32,7 +30,7 @@ def documentation_cmd(client, message, args):
 def test_command(client, message, args):
     return 'testing, args: ' + args
 
-	
+
 def bill_cmd(client, message, args):
     return 'http://belikebill.azurewebsites.net/billgen-API.php?default=1&name={}&sex=m' \
         .format(quote_plus(message.author.display_name))
