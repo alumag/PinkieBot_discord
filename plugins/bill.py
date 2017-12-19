6be02bed711653027e443b67118ccfbe7852ca11
Meitar Reihan <meitarr013@gmail.com>
2017-12-20 01:18:48 +0200

    added bill cmd

---
+++ plugins/bill.py
@@ -0,0 +1,13 @@
+from urllib.parse import quote_plus
+from discord import Message
+
+from cybot import utils, client
+
+
+@utils.register_command(name='bill', channels=['bot'])
+async def bill_cmd(message: Message, args: [str]):
+    """
+    bill: generate a bill meme with your name
+    """
+    await client.send_message(message.channel, 'http://belikebill.azurewebsites.net/billgen-API.php?default=1&name='
+                                               '{}&sex=m'.format(quote_plus(message.author.display_name)))
