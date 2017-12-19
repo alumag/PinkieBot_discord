788f535a81cec0fd0574f676e27d42d2b1b5a9cb
Aluma Gelbard <agelbard@twistbioscience.com>
2017-12-19 22:18:33 +0200

    add cybot package, add settings file

---
+++ cybot/settings.py
@@ -0,0 +1,15 @@
+import sys
+
+import discord
+
+# find the token and parse it
+TOKEN_PATH = 'token.txt'
+TOKEN = open(TOKEN_PATH).read().strip('\n')
+
+# run the client
+client = discord.Client()
+
+user_kick_timeout = 700
+eng = "poiuytrewqlkjhgfdsamnbvcxz"
+heb = "פםןוטארק'/ךלחיעכגדשצמנהבסז"
+
