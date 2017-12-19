a08dd89fc27fff23bb2a54cd837f0da9e481e7d1
Meitar Reihan <meitarr013@gmail.com>
2017-12-20 01:27:53 +0200

    adjusted urban cmd

--- plugins/urban_cmd.py
+++ plugins/urban_cmd.py
@@ -1,13 +1,24 @@
+from urllib.parse import quote_plus
 import urllib.request
 import json
 
+from discord import Message
+from cybot import utils, client
 
-def query_urban_dictionary(message, args):
-	try:
-		args = urllib.parse.quote(args)
-		query_data = urllib.request.urlopen(f'http://api.urbandictionary.com/v0/define?term={args}').read()
-		decoded_data = json.loads(query_data)
 
-		return f'**Word:** {decoded_data["list"][0]["word"]}\n {decoded_data["list"][0]["definition"]}'
-	except:
-		return "Sorry, I couldn't find anything on that"
+@utils.register_command(name='urban')
+async def query_urban_dictionary(message: Message, args: [str]):
+    """
+    urban PHASE: search PHASE on urban (a dictionary)
+    """
+    ans = "Sorry, I couldn't find anything on that"
+    if len(args) > 0:
+        try:
+            args = quote_plus(' '.join(args))
+            query_data = urllib.request.urlopen(f'http://api.urbandictionary.com/v0/define?term={args}').read()
+            decoded_data = json.loads(query_data)
+
+            ans = f'**Word:** {decoded_data["list"][0]["word"]}\n{decoded_data["list"][0]["definition"]}'
+        except:
+            pass
+    await client.send_message(message.channel, ans)
