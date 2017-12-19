d078b49d3528f1501d37425c9ab7c8d745be10f6
Meitar Reihan <meitarr013@gmail.com>
2017-12-20 01:55:07 +0200

    adjusted ord, chr

--- plugins/char_cmds.py
+++ plugins/char_cmds.py
@@ -1,24 +1,45 @@
-def ord_cmd(message, args):
+from discord import Message
+
+from cybot import utils, client
+
+
+@utils.register_command(name='ord', channels=['bot'])
+async def ord_cmd(message: Message, args: [str]):
+    """
+    ord CHAR: display the ascii value of a CHAR
+    """
     if len(args) != 1:
-        return 'Illegal'
-    try:
-        dec = ord(args)
-        hexa = hex(dec)
-        return "'" + args + "' = " + str(dec) + " dec / " + hexa + " hex"
-    except:
-        return 'Illegal'
-
-
-def chr_cmd(message, args):
-    if 'x' in args:
+        ans = 'Illegal'
+    else:
         try:
-            args = str(int(args, 16))
+            dec = ord(args[0])
+            hexa = hex(dec)
+            ans = "'" + args[0] + "' = " + str(dec) + " dec / " + hexa + " hex"
         except:
-            return 'Illegal'
-    if len(args) < 2 or not args.isdigit():
-        return 'Illegal'
+            ans = 'Illegal'
+    await client.send_message(message.channel, ans)
 
-    try:
-        return 'chr({}) = {}'.format(args, chr(int(args)))
-    except:
-        return '123'
+
+@utils.register_command(name='chr', channels=['bot'])
+async def chr_cmd(message, args):
+    """
+    chr NUM: convert NUM to a char
+    """
+    if len(args) != 1:
+        ans = 'Illegal'
+    else:
+        args = args[0]
+        if 'x' in args:
+            try:
+                args = str(int(args, 16))
+            except:
+                ans = 'Illegal'
+        if len(args) < 2 or not args.isdigit():
+            ans = 'Illegal'
+
+        try:
+            ans = 'chr({}) = {}'.format(args, chr(int(args)))
+        except:
+            ans = '123'
+    await client.send_message(message.channel, ans)
+
