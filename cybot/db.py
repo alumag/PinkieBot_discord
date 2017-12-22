5cb0c8fc9678e51945d3756d6b6a3d2db9f7a803
Aluma Gelbard <agelbard@twistbioscience.com>
2017-12-22 12:38:33 +0200

    add RunCommand class

--- cybot/db.py
+++ cybot/db.py
@@ -4,11 +4,26 @@ db handler of cybot
 
 import sqlite3  # we are using sqlite3 as our db
 
+# global connection and cursor
 conn = sqlite3.connect('cybot.db')
 c = conn.cursor()
 
 
+# create table wrapper
 def create_table(name: str):
+    """
+    usage:
+        @create_table(name='table_name')
+        def func():
+            return [
+                {"name": "field_name", "type": "field_type"},
+            ]
+        func
+    func should return list of fields
+    :param name: name of table
+    :return: bool
+    """
+
     def create(name: str, fields: [dict]):
         """
         fields = [{'name': , 'type':}, ..]
@@ -20,6 +35,7 @@ def create_table(name: str):
         except Exception as e:
             print(e)
             return False
+        print(f"table {name} created")
         return True
 
     def wrapper(func):
@@ -27,6 +43,31 @@ def create_table(name: str):
         return create(name, fields)
 
     return wrapper
+
+
+class RunCommand:
+    """
+    RunCommand should NEVER get user inputs
+    """
+    @staticmethod
+    def add_row(table: str, values: [str]):
+        values = ", ".join(values)
+        try:
+            c.execute(f"INSERT INTO {table} VALUES ({values})")
+        except Exception as e:
+            print(e)
+            return False
+        return True
+
+    @staticmethod
+    def update_row(table: str, expression: str, set: str):
+        try:
+            c.execute(f"UPDATE {table} SET {set} WHERE {expression}")
+        except Exception as e:
+            print(e)
+            return False
+        return True
+
 
 
 def InitDB():
@@ -37,7 +78,18 @@ def InitDB():
             {"name": "karma", "type": "real"}
         ]
 
+    @create_table(name='store')
+    def store():
+        return [
+            {"name": "name", "type": "text"},
+            {"name": "type", "type": "text"},
+            {"name": "value", "type": "text"},
+            {"name": "price", "type": "real"}
+        ]
+
     karma
+    store
+    conn.commit()
 
 
 InitDB()
